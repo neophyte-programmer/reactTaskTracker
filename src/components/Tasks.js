f21049d5d9a8436f69c6cb8a3065f3a3ec0bc1fd
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-11-20 17:43:12 +0000

    update

---
+++ src/components/Tasks.js
@@ -0,0 +1,32 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+
+    {
+        id: 2,
+        text: 'Hair Appointment',
+        day: 'Feb 8th at 10:30am',
+        reminder: true,
+    },
+
+    {
+        id: 3,
+        text: 'School Meeting',
+        day: 'Feb 9th at 4:30pm',
+        reminder: false,
+    },
+]
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
