3aabba6fc04a4ac99b9ca0a70b6e2da3561b2f12
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-11-20 17:30:54 +0000

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = () => {
+const Header = ({title}) => {
     return (
-        <div>
-            <h1>Task Tracker</h1>
+        <div className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' />
         </div>
     )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
