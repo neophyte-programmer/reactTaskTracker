dcf1d8b7d15dffe6f081488be2f1e33c9f2f44e0
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-11-21 12:42:09 +0000

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Tasks from "./components/Tasks";
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -48,8 +49,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />  
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />  
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log('click')
-    }
+const Header = ({title, onAdd, showAdd}) => {
+    
 
     
     return (
-        <div className='header'>
+        <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick} />
-        </div>
+            <Button 
+            color={showAdd ? 'red' : 'green' } 
+            text={showAdd ? 'Close' : 'Add'} 
+            onClick={onAdd} 
+            />
+        </header>
     )
 }
 
