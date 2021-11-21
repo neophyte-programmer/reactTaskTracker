7730ed9febd7fc48fcc4f710a20a32e8458a4497
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-11-21 11:43:01 +0000

    update

--- src/App.js
+++ src/App.js
@@ -28,6 +28,14 @@ function App() {
     },
 ])
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -41,7 +49,7 @@ function App() {
   return (
     <div className="container">
       <Header />  
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   );
